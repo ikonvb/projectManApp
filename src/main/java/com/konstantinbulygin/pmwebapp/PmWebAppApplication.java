c343d36c577ced4536e19b6c63485ed3ef1b564a
kostyaFrom <korustlt@gmail.com>
2021-03-11 21:13:25 +0400

    init commit

---
+++ src/main/java/com/konstantinbulygin/pmwebapp/PmWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.konstantinbulygin.pmwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/konstantinbulygin/pmwebapp/PmWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konstantinbulygin.pmwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
