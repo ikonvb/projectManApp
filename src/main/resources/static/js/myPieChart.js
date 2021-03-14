6da270507fc0b815458ea73b28af603795cf5a53
kostyaFrom <korustlt@gmail.com>
2021-03-14 17:49:36 +0400

    handle js code to display info into the chart

--- src/main/resources/static/js/myPieChart.js
+++ src/main/resources/static/js/myPieChart.js
@@ -1,16 +1,39 @@
+//chartData coming from home template
+let niceChartData = decodeHtml(chartData);
+let chartJsonArray = JSON.parse(niceChartData);
+
+let numericData = [];
+let labelData = [];
+
+for (let i = 0; i < chartJsonArray.length; i++) {
+    numericData[i] = chartJsonArray[i].value;
+    labelData[i] = chartJsonArray[i].label;
+}
+
 new Chart(document.getElementById("myPieChart"), {
     type: 'pie',
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March'],
+        labels: labelData,
         datasets: [{
             label: 'My First dataset',
             backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
-            borderColor: 'rgb(255, 99, 132)',
-            data: [2, 10, 5]
+            data: numericData
         }]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+        title: {
+            display: true,
+            text: 'Project Statuses'
+        }
+    }
 });
+
+//return readable json
+function decodeHtml(html) {
+    let txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
