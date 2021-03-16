228c90f82ebda9947b4c19b6dee70b905cc8ece4
kostyaFrom <korustlt@gmail.com>
2021-03-16 19:23:38 +0400

    added HttpRequestTest

---
+++ src/test/java/com/konstantinbulygin/pmwebapp/controllers/HttpRequestTest.java
@@ -0,0 +1,29 @@
+package com.konstantinbulygin.pmwebapp.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void homePageReturnVersionNumberCorrectlyThenSuccess() {
+
+        String renderedHtml = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+        assertTrue(renderedHtml.contains("3.0.0"));
+    }
+}
