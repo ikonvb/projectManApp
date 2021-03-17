0e5d9a6d8fd8aaa8e588dad20af7e73e69766327
kostyaFrom <korustlt@gmail.com>
2021-03-17 20:52:53 +0400

    added HttpSecurity config

--- src/main/java/com/konstantinbulygin/pmwebapp/security/SecurityConfig.java
+++ src/main/java/com/konstantinbulygin/pmwebapp/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.konstantinbulygin.pmwebapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -21,8 +22,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("kostya")
                 .password("pass")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("manager")
+                .password("pass")
+                .roles("ADMIN");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/employees/new").hasRole("ADMIN")
+                .antMatchers("/").authenticated()
+                .and()
+                .formLogin();
+    }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
